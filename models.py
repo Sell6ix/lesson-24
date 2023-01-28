23d1ba4b0ec545bc18811efe4fe75ef419574bf5
Sell6ix <110632259+Sell6ix@users.noreply.github.com>
2023-01-28 21:41:02 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,29 @@
+from typing import Iterable, Any
+
+from marshmallow import Schema, fields, validates_schema, ValidationError
+
+VALID_CMD_PARAMS: Iterable = (
+    'filter',
+    'sort',
+    'map',
+    'unique',
+    'limit',
+    'regex',
+
+)
+
+
+class RequestParams(Schema):
+    cmd = fields.Str(required=True)
+    value = fields.Str(required=True)
+
+    @validates_schema
+    def validate_cmd_params(self, values: dict[str, str], *args: Any, **kwargs: Any) -> dict[str, str]:
+        if values['cmd'] not in VALID_CMD_PARAMS:
+            raise ValidationError('"cmd"contains invalid value')
+        return values
+
+
+class BatchRequestParams(Schema):
+    queries = fields.Nested(RequestParams, many=True)
+
